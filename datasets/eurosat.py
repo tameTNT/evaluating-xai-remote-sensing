2cdb41b309df8338c90a86867ae3817b5eede4e5
tameTNT <30503695+tameTNT@users.noreply.github.com>
2024-11-06 15:25:54 +0000

    🚚 Rename eurosat dataset loader file and add additional options

---
+++ datasets/eurosat.py
@@ -0,0 +1,31 @@
+import logging
+import typing as t
+
+import torch
+import torchgeo.datasets
+
+import datasets.dataset_core
+
+DATASET_ROOT = datasets.dataset_core.get_dataset_root()
+logger = logging.getLogger("projectLog")
+
+
+def get_dataset(
+        split: t.Literal["train", "test", "val"],
+        bands: t.Sequence[str] = ('B04', 'B03', 'B02'),  # defaults to RGB bands
+        transforms: t.Optional[t.Callable[[t.Dict[str, torch.Tensor]], t.Dict[str, torch.Tensor]]] = None,
+        download=False,
+) -> torchgeo.datasets.EuroSAT:
+    """
+    Get the EuroSAT dataset using torchgeo (can download if necessary).
+
+    Defaults to returning only the RGB bands ('B04', 'B03', 'B02'). But supports all 13 bands.
+    """
+    logger.debug("Using `torchgeo.datasets` to load EuroSAT dataset.")
+    return torchgeo.datasets.EuroSAT(
+        root=str(DATASET_ROOT / "eurosat"),
+        split=split,
+        bands=bands,
+        transforms=transforms,
+        download=download
+    )
