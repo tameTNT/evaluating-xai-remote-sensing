7e8dbf4b693073eb8176f3a0fc7cc4e94c540951
tameTNT <30503695+tameTNT@users.noreply.github.com>
2024-11-06 09:49:20 +0000

    🌱 Update dataset root environment loading process
    Offers option to load DATASET_ROOT variable first before env.json

--- datasets/dataset_core.py
+++ datasets/dataset_core.py
@@ -1,18 +1,25 @@
 from pathlib import Path
 import json
+import os
+import logging
 
 def get_dataset_root() -> Path:
     """
     Get the dataset root directory from the local env.json file.
     Handles the case where the file is not found or the key is missing.
     """
-    env_file = Path.cwd() / "env.json"
-    try:
-        env_settings = json.load(env_file.open("r"))
-        dataset_root = Path(env_settings["dataset_root"])
-    except FileNotFoundError:
-        raise FileNotFoundError(f"Please create an env.json file (looking for {env_file.resolve()}).")
-    except KeyError:
-        raise KeyError(f"Please ensure that {env_file.resolve()} contains a `dataset_root` key.")
-
-    return dataset_root
+    dataset_root = os.getenv('DATASET_ROOT')  # first try to load from environment variables
+    if dataset_root is None:  # try to load from env.json instead
+        env_file = Path.cwd() / "env.json"
+        try:
+            env_settings = json.load(env_file.open("r"))
+            dataset_root = env_settings["dataset_root"]
+        except FileNotFoundError:
+            raise FileNotFoundError(f"Please create an env.json file (looking for {env_file.resolve()}).")
+        except KeyError:
+            raise KeyError(f"Please ensure that {env_file.resolve()} contains a `dataset_root` key.")
+    path = Path(dataset_root)
+    if path.is_dir():
+        return Path(dataset_root)
+    else:
+        raise FileNotFoundError(f"The dataset root was set successfully but doesn't actually exist: {path.resolve()} is not a directory.")
