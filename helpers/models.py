3ca4567692819b48092eace9b0db48051629d7db
Luca <30503695+tameTNT@users.noreply.github.com>
2025-02-09 13:20:40 +0000

    Remove inplace argument from ReLU functions
    To allow for autograd in SHAP module hopefully?

--- helpers/models.py
+++ helpers/models.py
@@ -111,14 +111,14 @@ class BasicBlock(nn.Module):
         # Both self.conv1 and self.downsample layers downsample the input when stride != 1
         self.conv1 = conv3x3(inplanes, planes, stride)
         self.bn1 = norm_layer(planes)
-        self.relu = nn.ReLU(inplace=True)
+        self.relu = nn.ReLU()
         self.conv2 = conv3x3(planes, planes)
         self.bn2 = norm_layer(planes)
         self.downsample = downsample
         self.stride = stride
 
         # Added separate ReLU for compatability with DeepLiftSHAP in Captum
-        self.relu2 = nn.ReLU(inplace=True)
+        self.relu2 = nn.ReLU()
 
     def forward(self, x: Tensor) -> Tensor:
         identity = x
@@ -171,13 +171,13 @@ class Bottleneck(nn.Module):
         self.bn2 = norm_layer(width)
         self.conv3 = conv1x1(width, planes * self.expansion)
         self.bn3 = norm_layer(planes * self.expansion)
-        self.relu = nn.ReLU(inplace=True)
+        self.relu = nn.ReLU()
         self.downsample = downsample
         self.stride = stride
 
         # Added separate ReLUs for compatability with DeepLiftSHAP in Captum
-        self.relu2 = nn.ReLU(inplace=True)
-        self.relu3 = nn.ReLU(inplace=True)
+        self.relu2 = nn.ReLU()
+        self.relu3 = nn.ReLU()
 
     def forward(self, x: Tensor) -> Tensor:
         identity = x
@@ -235,7 +235,7 @@ class ResNet(nn.Module):
         self.base_width = width_per_group
         self.conv1 = nn.Conv2d(3, self.inplanes, kernel_size=7, stride=2, padding=3, bias=False)
         self.bn1 = norm_layer(self.inplanes)
-        self.relu = nn.ReLU(inplace=True)
+        self.relu = nn.ReLU()
         self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
         self.layer1 = self._make_layer(block, 64, layers[0])
         self.layer2 = self._make_layer(block, 128, layers[1], stride=2, dilate=replace_stride_with_dilation[0])
