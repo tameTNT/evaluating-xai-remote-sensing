43e679c6fdc564bc5f90e6e9c5db2ff526899ded
tameTNT <30503695+tameTNT@users.noreply.github.com>
2025-02-18 21:26:52 +0000

    💄 Use jet in visualising importance in line with gradcam

--- helpers/plotting.py
+++ helpers/plotting.py
@@ -95,7 +95,7 @@ def visualise_importance(
     """
 
     show_image(x)
-    plt.imshow(importance_rank, alpha=alpha, cmap="inferno_r")
+    plt.imshow(importance_rank, alpha=alpha, cmap="jet_r")
 
     cb = plt.colorbar(label="Importance Rank (0 = most important)")
     cb.ax.invert_yaxis()
