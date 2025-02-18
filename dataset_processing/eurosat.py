7071a45a5ca3ee62b875056a482dd1084e286844
tameTNT <30503695+tameTNT@users.noreply.github.com>
2025-02-18 21:04:53 +0000

    Add common form of EuroSAT rgb function to dataset_processing
    Update old calls to function with new clearer name

--- dataset_processing/eurosat.py
+++ dataset_processing/eurosat.py
@@ -3,6 +3,7 @@ import typing as t
 
 import torch
 import torchgeo.datasets
+import torchvision.transforms.v2 as transforms
 
 import dataset_processing.core
 
@@ -10,7 +11,7 @@ DATASET_ROOT = dataset_processing.core.get_dataset_root()
 logger = logging.getLogger("projectLog")
 
 
-def get_dataset(
+def get_base_dataset(
         split: t.Literal["train", "test", "val"],
         bands: t.Sequence[str] = ('B04', 'B03', 'B02'),  # defaults to RGB bands
         transforms: t.Optional[t.Callable[[t.Dict[str, torch.Tensor]], t.Dict[str, torch.Tensor]]] = None,
@@ -28,4 +29,25 @@ def get_dataset(
         bands=bands,
         transforms=transforms,
         download=download
+    )
+
+
+def get_standard_rgb(split: t.Literal["test", "val"]) -> torchgeo.datasets.EuroSAT:
+    base_transforms = transforms.Compose([
+        transforms.ToImage(),
+        transforms.ToDtype(torch.float32, scale=False),  # scaling handles by normalise below
+        dataset_processing.core.RSNormaliseTransform(0, 2750),
+        # normalise to [0, 1] (based on maximums used in original paper)
+        transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5], inplace=True),  # shift to mean 0 and std 1
+
+        # scale as expected by ResNet (see https://pytorch.org/vision/stable/models/generated/torchvision.models.resnet50.html#torchvision.models.ResNet50_Weights)
+        # transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+
+        transforms.Resize(224, interpolation=transforms.InterpolationMode.BILINEAR),
+    ])
+    wrapped_base_transforms = dataset_processing.core.tensor_dict_transform_wrapper(base_transforms)
+
+    return get_base_dataset(
+        split, bands=("B04", "B03", "B02"),  # RGB bands
+        transforms=wrapped_base_transforms, download=False
     )
