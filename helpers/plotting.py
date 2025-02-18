e6d5a6cbcb5aa3c9f2bccbd43ad9faa32fb4f5cd
tameTNT <30503695+tameTNT@users.noreply.github.com>
2025-02-18 17:18:40 +0000

    🧹 Clean up plotting.py and add importance visualisation function

--- helpers/plotting.py
+++ helpers/plotting.py
@@ -1,23 +1,37 @@
+import typing as t
+
 import einops
 import matplotlib.pyplot as plt
 import numpy as np
 import torch
+from jaxtyping import Float, Int
 
 
-def show_sample_of_incorrect(idx_where_wrong, dataset, dataset_classes, test_predictions, test_labels):
+def show_sample_of_incorrect(
+        idx_where_wrong,
+        dataset,
+        dataset_classes,
+        test_predictions,
+        test_labels
+):
     np.random.seed(69)
     random_wrong_idxs = np.random.choice(idx_where_wrong, size=25, replace=False)
     fig = plt.figure(figsize=(10, 10), tight_layout=True)
     for i, idx in enumerate(random_wrong_idxs):
         ax = plt.subplot(5, 5, i + 1)
-        plt.title(f"'{dataset_classes[int(test_predictions[idx])]}'\n({dataset_classes[int(test_labels[idx])]})")
+        plt.title(f"'{dataset_classes[int(test_predictions[idx])]}'"
+                  f"\n({dataset_classes[int(test_labels[idx])]})")
         plt.imshow(einops.rearrange(dataset[idx]["image"], "c h w -> h w c"))
         plt.axis("off")
 
     return fig
 
 
-def plot_image(predictions_array, true_label, img):
+def plot_image_with_annotation(
+        predictions_array,
+        true_label,
+        img
+):
     plt.grid(False)
     plt.xticks([])
     plt.yticks([])
@@ -45,3 +59,46 @@ def plot_pred_bars(predictions_array: torch.Tensor, true_label):
 
     thisplot[predicted_label].set_color("orange")
     thisplot[true_label].set_color("green")
+
+
+def show_image(
+        img: t.Union[Float[torch.Tensor, "channels height width"],
+        Float[np.ndarray, "channels height width"]],
+        **kwargs
+):
+    """
+    Show an image using matplotlib.
+    The image is expected to be normalised (i.e. in the range [-1, 1])
+    and with dimensions (C, H, W).
+
+    kwargs are passed to `plt.imshow`.
+    """
+
+    if isinstance(img, torch.Tensor):
+        img = img.numpy(force=True)
+
+    if img.min() < 0:
+        img = (img + 1) / 2  # un-normalise
+    img = np.transpose(img, (1, 2, 0))  # move colour channel to end
+    plt.imshow(img, **kwargs)
+    plt.axis("off")
+
+
+def visualise_importance(
+        x: Float[np.ndarray, "batch_size height width channels"],
+        importance_rank: Int[np.ndarray, "batch_size height width"],
+        alpha: float = 0.2,
+):
+    """
+    Overlay (with transparency `alpha`) the importance rank over the image with
+    a colour bar.
+    """
+
+    show_image(x)
+    plt.imshow(importance_rank, alpha=alpha, cmap="inferno_r")
+
+    cb = plt.colorbar()
+    cb.ax.invert_yaxis()
+    _ = cb.solids.set(alpha=1)
+
+    plt.show()
