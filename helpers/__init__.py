9f4ce47e4e7e2829e8d06ae664f5116b835921d6
tameTNT <30503695+tameTNT@users.noreply.github.com>
2025-03-02 14:53:41 +0000

    ✨ Add spearman_rank function to similarity_metrics.py

--- helpers/__init__.py
+++ helpers/__init__.py
@@ -3,3 +3,4 @@ from . import ml
 from . import models
 from . import plotting
 from . import xai
+from . import similarity_metrics
--- helpers/similarity_metrics.py
+++ helpers/similarity_metrics.py
@@ -1,3 +1,12 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from scipy.stats import spearmanr
+from jaxtyping import Float
+import torch
+import einops
+import pandas as pd
+
+
 # for SHAP values this is better evaluated with L2 (hamming doesn't make sense
 # (not the same))
 def pixel_l2_distance_per_label(
@@ -62,3 +71,42 @@ def make_l2_distance_per_label_df(
     df = pd.DataFrame(l2_dist.numpy(force=True), index=class_names, columns=["L2 distance"])
     df.index.name = "Class"
     return df
+
+
+def spearman_rank(
+        x_original: Float[np.ndarray, "batch_size height width channels num_labels"],
+        x_new: Float[np.ndarray, "batch_size height width channels num_labels"],
+        show_plot: bool = False,
+) -> Float[np.ndarray, "batch_size"]:
+    """
+    Calculate the Spearman rank correlation between two sets of
+    images/explanations.
+
+    Optionally also show the scatter plots of the rankings for each image.
+    """
+
+    assert x_original.shape == x_new.shape, "x1_ranked and x2_ranked must have the same shape"
+
+    num_x = x_original.shape[0]
+
+    spearman_coeffs = np.zeros(0)
+
+    if show_plot:
+        fig, axes = plt.subplots(1, num_x, figsize=(10, 10))
+    for i, (og, new) in enumerate(zip(x_original, x_new)):
+        og_flat, new_flat = og.flatten(), new.flatten()
+        spearman_coeffs = np.append(spearman_coeffs, spearmanr(og_flat, new_flat).statistic)
+
+        if show_plot:
+            ax = axes[i]
+            ax.set_title(f"SCC={spearmanr(og_flat, new_flat).statistic:.3f}")
+            ax.scatter(og_flat, new_flat)
+            ax.set_xlabel("Ranking for original")
+            ax.set_ylabel("Ranking for new")
+            ax.plot(range(og.max()), "r--")
+            ax.set_aspect("equal")
+
+    if show_plot:
+        fig.tight_layout()
+
+    return spearman_coeffs
