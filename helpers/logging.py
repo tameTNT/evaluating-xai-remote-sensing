7aa1bddc6e96f95955ee6a898e5a2dbd9a47095f
tameTNT <30503695+tameTNT@users.noreply.github.com>
2025-03-03 12:28:35 +0000

    🐛 Fix multiple logger file handlers being added
    Wrote the same log entry several times - once for each handler.
    Now checks whether handlers have already been attached.

--- helpers/logging.py
+++ helpers/logging.py
@@ -10,26 +10,30 @@ LOG_DIR.mkdir(parents=False, exist_ok=True)
 def get_logger(name: str) -> logging.Logger:
     # Basic setup mirrors https://docs.python.org/3/howto/logging-cookbook.html#using-logging-in-multiple-modules
     logger = logging.getLogger(name)
-    logger.setLevel(logging.DEBUG)
+    if not logger.handlers:  # avoid adding handlers multiple times
+        logger.setLevel(logging.DEBUG)
 
-    # create file handler which logs even debug messages
-    fh = logging.FileHandler(LOG_DIR / f"{name}.log", mode="a")
-    fh.setLevel(logging.DEBUG)
+        # create file handler which logs even debug messages
+        fh = logging.FileHandler(LOG_DIR / f"{name}.log", mode="a")
+        fh.setLevel(logging.DEBUG)
 
-    # create console handler with a higher log level
-    ch = logging.StreamHandler()
-    ch.setLevel(logging.ERROR)
+        # create console handler with a higher log level
+        ch = logging.StreamHandler()
+        ch.setLevel(logging.ERROR)
 
-    # create formatter and add it to the handlers
-    formatter = logging.Formatter(
-        "%(asctime)s | %(name)s | %(levelname)s | %(pathname)s | %(funcName)s | %(message)s"
-    )
-    fh.setFormatter(formatter)
-    ch.setFormatter(formatter)
+        # create formatter and add it to the handlers
+        formatter = logging.Formatter(
+            "%(asctime)s | %(name)s | %(levelname)s | %(pathname)s | %(funcName)s | %(message)s"
+        )
+        fh.setFormatter(formatter)
+        ch.setFormatter(formatter)
 
-    # add the handlers to the logger
-    logger.addHandler(fh)
-    logger.addHandler(ch)
+        # add the handlers to the logger
+        logger.addHandler(fh)
+        logger.addHandler(ch)
 
-    logger.debug(f"Logger successfully initialised at {fh.baseFilename}")
+        logger.debug(f"New logger successfully initialised at {fh.baseFilename}")
+    else:
+        logger.debug(f"Logger successfully returned from {logger.handlers[0].baseFilename}")
+
     return logger
