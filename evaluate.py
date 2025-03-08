606150f9c2e0de1a50cfce23db37f74f5ecb66d7
tameTNT <30503695+tameTNT@users.noreply.github.com>
2025-03-08 19:12:18 +0000

    ♻️ Update evaluate.py with new project idioms
    Use get_model_type, get_dataset_object and so on

--- evaluate.py
+++ evaluate.py
@@ -5,24 +5,45 @@ import safetensors.torch as st
 import torch
 
 import dataset_processing
+import helpers
 import models
 from evaluate_xai.correctness import Correctness
 from xai.shap_method import SHAPExplainer
 
-model_weights_path = Path('/home2/jgcw74/l3_project/checkpoints/resnet50/FineTunedResNet50_final_ft_weights(0.989).st')
-# todo: modularise get_model_type functions from train.py
-model_to_explain = ResNet50(
-    pretrained=False, n_input_bands=3, n_output_classes=10
+random_seed = 42
+dataset_name = "EuroSATRGB"
+normalisation_type = "scaling"
+use_resize = False
+batch_size = 32
+
+model_name = "ResNet50"
+num_workers = 4
+
+logger = helpers.log.get_logger("main")
+
+torch_device = helpers.utils.get_torch_device()
+torch.manual_seed(random_seed)
+
+model_type = models.get_model_type(model_name)
+dataset = dataset_processing.get_dataset_object(
+    dataset_name, "test", model_type.expected_input_dim,
+    normalisation_type=normalisation_type, use_resize=use_resize,
+    batch_size=batch_size, num_workers=num_workers, device=torch_device,
+)
+
+logger.info("Creating model and loading pretrained weights.")
+
+weights_paths = json.load(Path("weights_paths.json").open("r"))
+model_weights_path = Path(weights_paths[dataset_name][model_name]).resolve()
+
+model_to_explain = model_type(
+    pretrained=False, n_input_bands=dataset.N_BANDS, n_output_classes=dataset.N_CLASSES,
 )
 st.load_model(model_to_explain, model_weights_path)
 model_to_explain.eval()
+logger.info(f"Loaded weights from {model_weights_path} successfully.")
 
-random_seed = 42
-torch.manual_seed(random_seed)
 
-dataset = dataset_processing.eurosat.EuroSATRGB(
-    split="test", image_size=model_to_explain.expected_input_dim
-)
 imgs_to_explain = torch.stack(
     [dataset[i]["image"] for i in torch.randint(0, len(dataset), (5,))]
 )
