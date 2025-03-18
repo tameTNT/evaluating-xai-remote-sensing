d265cd2d66f5f1dc45e7645cc6b436bac838ac53
tameTNT <30503695+tameTNT@users.noreply.github.com>
2025-03-18 10:11:07 +0000

    🗂️  Update PatternNet dataset class with split idxs and transforms

--- dataset_processing/patternnet.py
+++ dataset_processing/patternnet.py
@@ -23,8 +23,11 @@ class PatternNet(PatternNetBase, dataset_processing.core.RSDatasetMixin):
         self.bands = ("B04", "B03", "B02")
         self.N_BANDS = len(self.bands)
 
-        scaling_transform = None
-        normalisation = None
+        scaling_transform = dataset_processing.core.RSScalingTransform(
+            input_min=0., input_max=255., clamp=False
+        )
+        normalisation = vision_transforms.Normalize(mean=[0.5] * self.N_BANDS,
+                                                    std=[0.5] * self.N_BANDS, inplace=True)
 
         # Add randomised transforms
         augmentations = None
@@ -42,28 +45,31 @@ class PatternNet(PatternNetBase, dataset_processing.core.RSDatasetMixin):
         self.N_CLASSES = len(self.classes)
 
         # Manually add dataset splits (not provided by torchgeo)
-        all_class_labels = [l for _, l in self.imgs]
+        all_class_labels = np.array([l for _, l in self.imgs])
 
         train_idxs, split_idxs = train_test_split(
-            np.arange(len(self.imgs)), test_size=0.4,
+            np.arange(len(all_class_labels)), test_size=0.4,
             stratify=all_class_labels, random_state=random_split_seed
         )
+
         val_idxs, test_idxs = train_test_split(
             split_idxs, test_size=0.5,
             stratify=all_class_labels[split_idxs], random_state=random_split_seed
         )
-        self.split_indexes = {  # 60%, 20%, 20% split
+        self.split_idxs = {  # 60%, 20%, 20% split
             "train": train_idxs,
             "val": val_idxs,
             "test": test_idxs
         }
+        # # replace self.imgs with only the imgs in the split
+        # self.imgs = self.imgs[self.split_idxs[self.split]]
 
     def __getitem__(self, idx):
-        split_idx = self.split_indexes[self.split][idx]
-        super().__getitem__(split_idx)
+        split_idx = self.split_idxs[self.split][idx]
+        return super().__getitem__(split_idx)
 
     def __len__(self):
-        return len(self.split_indexes[self.split])
+        return len(self.split_idxs[self.split])
 
     def get_original_train_dataloader(self, shuffle=False):
         return torch.utils.data.DataLoader(
