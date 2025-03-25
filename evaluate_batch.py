d9264537a7f0b5c0d9d4a22c72bf55e21192179e
tameTNT <30503695+tameTNT@users.noreply.github.com>
2025-03-25 20:38:31 +0000

    Begin to add metric calculations to evaluate_batch.py

--- evaluate_batch.py
+++ evaluate_batch.py
@@ -10,11 +10,17 @@ import safetensors.torch as st
 import numpy as np
 from tqdm.auto import tqdm
 import matplotlib.pyplot as plt
+import pandas as pd
 
 import models
 import dataset_processing
 import xai
-import evaluate_xai
+import evaluate_xai.deletion
+from evaluate_xai.correctness import Correctness
+from evaluate_xai.output_completeness import OutputCompleteness
+from evaluate_xai.continuity import Continuity
+from evaluate_xai.contrastivity import Contrastivity
+from evaluate_xai.compactness import Compactness
 import helpers
 
 torch_device = helpers.utils.get_torch_device()
@@ -161,6 +167,27 @@ if __name__ == "__main__":
         help="Method to use for deletion/perturbation-related evaluation methods. "
              "Defaults to 'shuffle'.",
     )
+    evaluation_group.add_argument(
+        "--similarity_intersection_k",
+        type=int,
+        default=5000,
+        help="Number of pixels to use for the intersection similarity metric. "
+             "Defaults to 5000.",
+    )
+    evaluation_group.add_argument(
+        "--deletion_iterations",
+        type=int,
+        default=15,
+        help="Number of iterations to use for the incremental methods. "
+             "Defaults to 15.",
+    )
+    evaluation_group.add_argument(
+        "--num_random_trials",
+        type=int,
+        default=5,
+        help="Number of random trials to use for the methods with a random comparison. "
+             "Defaults to 5.",
+    )
 
     args: argparse.Namespace = parser.parse_args()
     print("Got args:", args, "\n")
@@ -177,6 +204,9 @@ if __name__ == "__main__":
 
     samples_per_class: int = args.samples_per_class
     deletion_method: evaluate_xai.deletion.METHODS = args.deletion_method
+    similarity_intersection_k: int = args.similarity_intersection_k
+    deletion_iterations: int = args.deletion_iterations
+    num_random_trials: int = args.num_random_trials
     
     logger.info(f"Successfully got script arguments: {args}.")
 
@@ -184,6 +214,13 @@ if __name__ == "__main__":
     np_rng = np.random.default_rng(random_seed)
 
     dataset, model_to_explain = get_data_and_model()
+
+    results_df = pd.DataFrame(columns=pd.MultiIndex.from_tuples(
+        [("correctness", "model_randomisation"), ("correctness", "incremental_deletion"),
+         ("output_completeness", "deletion_check"), ("output_completeness", "preservation_check"),
+         ("continuity", "perturbation"), ("contrastivity", "adversarial_attack"),
+         ("compactness", "threshold")]
+    ), index=range(dataset.N_CLASSES))
 
     classes = np.array([class_ for _, class_ in dataset.imgs])
     for c in tqdm(range(dataset.N_CLASSES), ncols=110, desc="xAI per class"):
@@ -200,12 +237,39 @@ if __name__ == "__main__":
 
         explainers_for_c = generate_explanations(class_idxs_sampled, c)
         # Combine all the explainers for this class into one
-        e = functools.reduce(lambda x, y: x | y, explainers_for_c)
+        combined_exp = functools.reduce(lambda x, y: x | y, explainers_for_c)
 
-        helpers.plotting.visualise_importance(e.input, e.ranked_explanation,
-                                              alpha=.2, with_colorbar=False)
-        plt.title(f"Class {c:02}, explainer idx 0")
-        plt.show()
+        # helpers.plotting.visualise_importance(combined_exp.input, combined_exp.ranked_explanation,
+        #                                       alpha=.2, with_colorbar=False)
+        # plt.title(f"Explanations for Class {c:02}")
+        # plt.show()
+
+        # ==== Evaluate Correctness ====
+        correctness = Correctness(combined_exp, max_batch_size=batch_size)
+
+        correctness_mr_dict = correctness.evaluate(
+            method="model_randomisation", visualise=False,
+        )(l2_normalise=True, intersection_k=similarity_intersection_k)
+        top_k_intersection_with_randomised: np.ndarray = correctness_mr_dict["top_k_intersection"]
+
+        correctness_id_dict = correctness.evaluate(
+            method="incremental_deletion",
+            deletion_method=deletion_method,
+            iterations=deletion_iterations, n_random_rankings=num_random_trials,
+            random_seed=random_seed, visualise=False,
+        )
+
+        # ==== Save all results in the dataframe ====
+        results_df.loc[c] = [
+            # We want the intersection to be low since the model was randomised
+            top_k_intersection_with_randomised.mean(),
+            # We expect the informed deletion to be smaller than the randomised one so this should be small
+            (correctness_id_dict["informed"]/correctness_id_dict["random"]).mean(),
+            # todo: include remaining metrics
+        ]
+
+    store = pd.HDFStore(str(Path("output.csv")))
+    store["results_df"] = results_df  # saves results_df object to HDF5 file
 
 else:
     raise RuntimeError("Please run this script from the command line.")
