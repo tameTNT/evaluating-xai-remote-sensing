f896122726e2313f3642be7457451eba961e7f45
tameTNT <30503695+tameTNT@users.noreply.github.com>
2025-03-28 12:09:31 +0000

    🔨 Update training args for GradCAM+PatternNet and some SHAP

--- slurm_ncc/evaluate_on_slurm.sh
+++ slurm_ncc/evaluate_on_slurm.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # The first argument to this script should be the name of job
 # The second argument should be the path to a file containing the arguments for evaluate_batch.py
+# The third argument should the number of hours to run the job for (usually 05 is fine)
 # Any remaining arguments are passed to evaluate_batch.py
 
 # work way up the path to get the relevant parameters
@@ -20,10 +21,10 @@ printf '%s\n' "#!/bin/bash" \
        "#SBATCH -p ug-gpu-small" \
        '#SBATCH --qos="short"' \
        "#SBATCH --mem=28G" \
-       "#SBATCH -t 00-05:00:00" \
+       "#SBATCH -t 00-$3:00:00" \
        "#SBATCH --gres=gpu:1g.10gb:1" \
        "#SBATCH --mail-user=jgcw74@durham.ac.uk" \
-       "#SBATCH --mail-type=BEGIN,END,FAIL" \
+       "#SBATCH --mail-type=FAIL" \
        "#SBATCH --job-name=$1" \
        "#SBATCH -o /home2/jgcw74/l3_project/.logs/slurm/$1.log" \
        "##Activate conda and environment" \
@@ -31,6 +32,6 @@ printf '%s\n' "#!/bin/bash" \
        "conda activate sat_project" \
        "cd ~/l3_project" \
        "export DATASET_ROOT=~/datasets" \
-       "$(printf '%s ' "python batch_evaluate.py --dataset_name $dataset_name --model_name $model_name --explainer_name $explainer_name $args ${@:3}")" > ~/l3_project/slurm_ncc/evaluate_temp.batch
+       "$(printf '%s ' "python batch_evaluate.py --dataset_name $dataset_name --model_name $model_name --explainer_name $explainer_name $args ${@:4}")" > ~/l3_project/slurm_ncc/evaluate_temp.batch
 
 sbatch ~/l3_project/slurm_ncc/evaluate_temp.batch
